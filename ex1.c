c7c7ddf63d5a516bc4a57c337ba5df027a0809fd
phamchidung342 <63303733+phamchidung342@users.noreply.github.com>
2023-08-25 10:48:54 +0700

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Entex the second number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum is: %d", sum);
+    return 0;
+}
